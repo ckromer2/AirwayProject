9b5c2dc0b7e4791077c582ddfbb4100eb8f41c4a
CreeperKiller1271 <CreeperKiller1271@users.noreply.github.com>
2023-03-17 21:14:49 -0400

    Added the main data classes. Changed the project organization a bit. Created the infrastucture for the non database elements.

--- GROUP9Project/DataBase/Flights.cs
+++ GROUP9Project/DataBase/Flights.cs
@@ -1,22 +1,66 @@
 ﻿using SQLite;
-using System.Runtime.CompilerServices;
+using System;
 
 namespace GROUP9Project.DataBase;
 
-public class Plane 
+public class Route
 {
-    //Defines the collums of the tables.
     [PrimaryKey, AutoIncrement]
-    public int PlaneId { get; set; }//Generic Id of this plane
-    public string PlaneName { get; set; }
-    public int Capacity { get; set; }
+    public int RouteId { get; private set; }
+    public int Start { get; private set; }
+    public int End { get; private set; }
+    public DayOfWeek SchedualDate { get; private set; }
+    public int SchedualTime { get; private set; }
+
+    public Route() 
+    {
+        Start = 0;
+        End = 0;
+        SchedualDate = 0;
+        SchedualTime = 0;
+    }
+
+    public Route(int s, int e, DayOfWeek sDay, int sTime)
+    { 
+        Start = s;
+        End = e;
+        SchedualDate = sDay;
+        SchedualTime = sTime;
+    }
 
 
 }
 
-public class Flights
-{ 
+public class Flight
+{
+    [PrimaryKey, AutoIncrement]
+    public int FlightId { get; private set; }
+    public int PlainId { get; private set; }
+    public int RouteId { get; private set; }
+    public DateTime FlightDate { get; private set; }
+
+    public Flight()
+    {
+        PlainId = 0;
+        RouteId = 0;
+        FlightDate = DateTime.MinValue;
+    }
+
+    public Flight(int pId, int rId, DateTime fDate)
+    {
+        PlainId = pId;
+        RouteId = rId;
+        FlightDate = fDate;
+    }
+
+
+    public void ChangePlane()
+    { 
     
+    
+
+    }
+
 
 }
 
---
+++ GROUP9Project/DataBase/PlanesAirportsDistances.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GROUP9Project.DataBase;
+
+public struct Airport
+{
+    public string AirportName { get; private set; }
+    public string AirportCode { get; private set; }
+
+    public Airport(string aName, string aCode)
+    {
+        AirportName = aName;
+        AirportCode = aCode;
+    }
+}
+
+public class Plane
+{
+    public string PlaneName { get; private set; }
+    public int Capacity { get; private set; }
+
+    public Plane(string PName, int Cap)
+    {
+        PlaneName = PName;
+        Capacity = Cap;
+    }
+
+}
+
+public class PlanesAirportsDistances
+{
+    public List<Airport> Airports { get; private set; }
+    public List<Plane> Planes { get; private set; }
+    public List<(int Start, int Destination)> Distances { get; private set; }
+
+    public PlanesAirportsDistances() 
+    {
+        //Constructs the list of Planes
+        Planes = new List<Plane>();
+        Planes.Add(new Plane("", 0));
+        //Constructs the list of Airports
+        Airports = new List<Airport>();
+        Airports.Add(new Airport("",""));
+        //Constructs the distanst graph
+        Distances = new List<(int Start, int Destination)>{ { }, { }, { } };
+    }
+
+}
