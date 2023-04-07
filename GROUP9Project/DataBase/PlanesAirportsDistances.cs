a2aef055b37bfa4fe109b31791d3ba9b76825ba9
CreeperKiller1271 <CreeperKiller1271@users.noreply.github.com>
2023-04-07 15:20:05 -0400

    Update PlanesAirportsDistances.cs
    Added the full distance array as well as the airport list.

--- GROUP9Project/DataBase/PlanesAirportsDistances.cs
+++ GROUP9Project/DataBase/PlanesAirportsDistances.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace GROUP9Project.DataBase;
 
-namespace GROUP9Project.DataBase;
+//Used to make sure the lists dont overrun the buffer size.
+enum AirportEnum {
+    CLE,
+    BNA,
+    TPA,
+    IAD,
+    LAX,
+    SEA,
+    JFK,
+    ORD,
+    MSP,
+    DFW,
+    DEN,
+    LAS,
+    DTW
+}
+
 
 public struct Airport
 {
@@ -36,18 +47,43 @@ public class PlanesAirportsDistances
 {
     public List<Airport> Airports { get; private set; }
     public List<Plane> Planes { get; private set; }
-    public List<(int Start, int Destination)> Distances { get; private set; }
+    public List<List<float>> Distances { get; private set; }
 
     public PlanesAirportsDistances() 
     {
         //Constructs the list of Planes
-        Planes = new List<Plane>();
-        Planes.Add(new Plane("", 0));
+        Planes = new List<Plane> {   };
         //Constructs the list of Airports
-        Airports = new List<Airport>();
-        Airports.Add(new Airport("",""));
+        Airports = new List<Airport> {  new Airport("Cleveland Hopkins International Airport", "CLE"),
+                                        new Airport("Nashville International Airport", "BNA"),
+                                        new Airport("Tampa International Airport", "TPA"),
+                                        new Airport("Washington Dulles Intl airport", "IAD"),
+                                        new Airport("Los Angeles International Airport", "LAX"),
+                                        new Airport("Seattle-Tacoma International Airport", "SEA"),
+                                        new Airport("John F. Kennedy International Airport", "JFK"),
+                                        new Airport("Chicago O'Hare International Airport", "ORD"),
+                                        new Airport("Minneapolis−Saint Paul International Airport", "MSP"),
+                                        new Airport("Dallas Fort Worth International Airport", "DFW"),
+                                        new Airport("Denver International Airport", "DEN"),
+                                        new Airport("Las Vegas International Airport", "LAS"),
+                                        new Airport("Detroit Metropolitan Wayne County Airport", "DTW")
+                                        };
+
         //Constructs the distanst graph
-        Distances = new List<(int Start, int Destination)>{ { }, { }, { } };
+        Distances = new List<List<float>> {   new List<float> { 0, 447.69f, 928.5f, 287.35f, 2049, 2016, 424, 315, 621, 1021, 1198, 1821, 96 },
+                                                                new List<float> { 447.69f, 0, 613, 541.29f, 1794, 1974, 765, 410, 696, 631, 1012, 1584, 457 },
+                                                                new List<float> { 928.5f, 613, 0, 812.43f, 2154, 2518, 1007, 1014, 1308, 928, 1506, 1981, 985 },
+                                                                new List<float> { 287.35f, 541.29f , 812.43f, 0, 2282.91f, 2300, 228, 588, 907, 1170, 1449, 2061, 383 },
+                                                                new List<float> { 2049, 1794 , 2154, 2282.91f, 0, 954.98f, 2470, 1742, 1534, 1233, 861, 237, 1975 },
+                                                                new List<float> { 2016, 1974, 2518, 2300, 954.98f, 0, 2415.04f, 1717, 1395, 1658, 1023, 867, 1922 },
+                                                                new List<float> { 424, 765, 1007, 228, 2470, 2415.04f, 0, 738.33f, 1027, 1389, 1622, 2263, 508 },
+                                                                new List<float> { 315, 410, 1014, 588, 1742, 1717, 738.33f, 0, 333.56f, 802, 887, 1511, 234 },
+                                                                new List<float> { 621, 696, 1308, 907, 1534, 1395, 1027, 333.56f, 0, 852.89f, 679.18f, 1298, 528 },
+                                                                new List<float> { 1021, 631, 928, 1170, 1233, 1658, 1389, 802, 852.89f, 0, 640.93f, 1054, 986 },
+                                                                new List<float> { 1198, 1012, 1506, 1449, 861, 1023, 1622, 887, 679.18f, 640.93f, 0, 626.85f, 1120.5f },
+                                                                new List<float> { 1821, 1584, 1981, 2061, 237, 867, 2263, 1511, 1298, 1054, 626.85f, 0, 1745.17f },
+                                                                new List<float> { 96, 457, 985, 383, 1975, 1922, 508, 234, 528, 986, 1120.5f, 1745.17f, 0 }
+                                                                };
     }
 
 }
