3399c48256aa74804d1e38e16ee559744e5e114e
ckromer2 <2020ckromer@gmail.com>
2023-04-18 18:01:32 -0400

    Added manager(controller) classes with some general methods which can be deleted or edited as seen fit. Can also add new methods

---
+++ GROUP9Project/Controllers/CustomerManager.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * By: Cobey Kromer
+ * Date: 4/16/23
+ * For: Controller class for actions taken by customers
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GROUP9Project.Controllers
+{
+    
+    public static class CustomerManager
+    {
+        public static void Login() 
+        {
+            // Make the ApplicationData.AppUser equal to something if the user logged in correctly
+        }
+
+        public static void Rewards()
+        {
+        }
+
+        public static void Booking()
+        {
+        }
+
+        public static void Cancellation()
+        {
+
+        }
+
+        public static void PrintBoardingPass()
+        {
+
+        }
+
+        public static void BrowseFlights()
+        {
+
+        }
+
+        public static void CreateAccount()
+        {
+
+        }
+
+    }
+
+    
+}
---
+++ GROUP9Project/Controllers/EmployeeManager.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * By: Cobey Kromer
+ * Date: 4/16/23
+ * For: Controller class for actions taken by employees
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GROUP9Project.Controllers
+{
+    public static class EmployeeManager
+    {
+        public static void ViewAccountInfo()
+        {
+
+        }
+
+        public static void PrintFlightManifest()
+        {
+
+        }
+
+        public static void FlightsToOffer()
+        {
+
+        }
+
+        public static void AssignPlanes()
+        {
+
+        }
+        
+    }
+}
