be48c41fe24006113b1479d372a885cd77f39c61
ckromer2 <2020ckromer@gmail.com>
2023-04-18 23:23:39 -0400

    Small fix

--- GROUP9Project/UI/LoginForm.cs
+++ GROUP9Project/UI/LoginForm.cs
@@ -12,15 +12,19 @@ namespace GROUP9Project
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            if (CustomerManager.Login(IdTextBox.Text, PasswordTextBox.Text))
+            // If all text boxes have stuff in them
+            if (IdTextBox.Text != "" && PasswordTextBox.Text != "")
             {
-                this.Hide();
-                UI.FlightsForm flightsForm = new UI.FlightsForm();
-                flightsForm.Closed += (s, args) => this.Close();
-                flightsForm.Show();
-                flightsForm.SetDesktopLocation(this.Location.X, this.Location.Y);
+                // If the customer manager allows a login
+                if (CustomerManager.Login(IdTextBox.Text, PasswordTextBox.Text))
+                {
+                    this.Hide();
+                    UI.FlightsForm flightsForm = new UI.FlightsForm();
+                    flightsForm.Closed += (s, args) => this.Close();
+                    flightsForm.Show();
+                    flightsForm.SetDesktopLocation(this.Location.X, this.Location.Y);
+                }
             }
-
 
         }
 
